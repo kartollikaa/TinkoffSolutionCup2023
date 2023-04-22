830b1552667882054efa8f862090c6d217ed8b92
Dmitrii Maksimov <dmitrij.maksimow@gmail.com>
2023-04-22 12:15:15 +0300

    Initial commit

---
+++ app/src/main/java/com/kartollika/myapplication/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.kartollika.myapplication
+
+import android.app.Activity
+
+class MainActivity : Activity() {
+
+  override fun setContentView(layoutResID: Int) {
+    super.setContentView(R.layout.main_activity)
+  }
+}
